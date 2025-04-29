c7ddce0cbc8a0eb022e74b940067519fa6db7b24
DaveyNavy <davidyzhu2@gmail.com>
2025-04-28 23:09:25 -0700

    Add database initialization and schemas for user and recipe

---
+++ backend/models/db_init.js
@@ -0,0 +1,35 @@
+import "dotenv/config";
+import { neon } from "@neondatabase/serverless";
+const sql = neon(process.env.DATABASE_URL);
+
+await sql.query(`CREATE TABLE IF NOT EXISTS users (
+    username VARCHAR(255) PRIMARY KEY,
+    password VARCHAR(255) 
+)`);
+
+await sql.query(`CREATE TABLE IF NOT EXISTS recipes (
+    id INTEGER UNIQUE GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255), 
+    author VARCHAR(255) REFERENCES users(username),
+    instructions TEXT
+)`);
+
+await sql.query(`CREATE TABLE IF NOT EXISTS ingredients (
+    id INTEGER UNIQUE GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255)
+)`);
+
+await sql.query(`CREATE TABLE IF NOT EXISTS recipe_ingredients (
+    recipe_id INTEGER REFERENCES recipes(id),
+    ingredient_id INTEGER REFERENCES ingredients(id)
+)`);
+
+await sql.query(`CREATE TABLE IF NOT EXISTS tags (
+    id INTEGER UNIQUE GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255)
+)`);
+
+await sql.query(`CREATE TABLE IF NOT EXISTS recipe_tags (
+    recipe_id INTEGER REFERENCES recipes(id),
+    tag_id INTEGER REFERENCES tags(id)
+)`);
