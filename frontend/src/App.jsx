699c0eb51f9e37814eb738a80a589c926eeb0ab6
louisquantran <louis.quantran@gmail.com>
2025-05-08 23:58:23 -0700

    Updated ProfilePage.jsx and App.jsx, Profile Page now can be tested for communication between frontend and backend. However, Neon connection needs to be set up correctly (maybe no env file?).

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,20 @@
-import { useState } from 'react'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HomePage from './HomePage';
 import MainPage from './MainPage';
 import RecipesPage from './RecipesPage';
 import ProfilePage from './ProfilePage';
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <Router>
       <nav>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/main">Main Page</Link>
-          </li>
-          <li>
-            <Link to="/recipes">Recipes Page</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile Page</Link>
-          </li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/main">Main Page</Link></li>
+          <li><Link to="/recipes">Recipes Page</Link></li>
+          <li><Link to="/profile/louis">Profile Page</Link></li>
         </ul>
       </nav>
 
@@ -31,22 +22,10 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/recipes" element={<RecipesPage />} />
-        <Route
-          path="/profile"
-          element={
-            <ProfilePage
-              username="Louis"
-              userStats={{ likes: 10, comments: 5, rating: 4.3 }}
-              uploadedRecipes={[
-                { id: 'r1', title: 'Pasta', date: '2025-05-07' },
-                { id: 'r2', title: 'Salad', date: '2025-05-08' },
-              ]}
-            />
-          }
-        />
+        <Route path="/profile/:id" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
