bf30510330b92c6224c654fc98be1f1434586e90
Phantom-101 <ccradical2468@gmail.com>
2025-05-13 00:36:46 -0700

    Revert back to 699c0eb51f9e37814eb738a80a589c926eeb0ab6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,48 +4,17 @@ import HomePage from './HomePage';
 import MainPage from './MainPage';
 import RecipesPage from './RecipesPage';
 import ProfilePage from './ProfilePage';
-import UploadPage from './UploadPage';
-import './App.css'
+import './App.css';
 
 function App() {
-  // Retrive Dylan token from local storage on initial load
-  const [token, setToken] = useState(localStorage.getItem('authToken') || null);
-  const [username, setUsername] = useState(localStorage.getItem('username') || '');
-
-  // Store Dylan token in local storage when it changes
-  useEffect(() => {
-      if (token) {
-          localStorage.setItem('authToken', token);
-          localStorage.setItem('username', username);
-      }
-  }, [token, username]);
-
-  const handleLogout = () => {
-      setToken(null);
-      setUsername('');
-      localStorage.removeItem('authToken');
-      localStorage.removeItem('username');
-  };
-
   return (
     <Router>
       <nav>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/main">Main Page</Link>
-          </li>
-          <li>
-            <Link to="/recipes">Recipes Page</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile Page</Link>
-          </li>
-          <li> 
-            <Link to="/upload">Upload Page</Link>
-          </li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/main">Main Page</Link></li>
+          <li><Link to="/recipes">Recipes Page</Link></li>
+          <li><Link to="/profile/louis">Profile Page</Link></li>
         </ul>
       </nav>
 
@@ -53,19 +22,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/recipes" element={<RecipesPage />} />
-        <Route path="/upload" element={<UploadPage />}/>
-        <Route
-          path="/profile/:id"
-          element={token ? (
-            <ProfilePage />
-          ) : (
-            <Navigate to="/login" />
-          )}
-        />
-        <Route
-          path="/login"
-          element={<LoginPage setToken={setToken} setUsername={setUsername} />}
-        />
+        <Route path="/profile/:id" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
